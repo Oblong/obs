10d796717d201ccfaf6f88d1136565fb84b69e31
Dan Kegel <dank@kegel.com>
2020-03-10 11:57:41 -0700

    fix-obs.sh: just use bootstrap-obs.sh

--- buildbot/fix-obs.sh
+++ buildbot/fix-obs.sh
@@ -5,10 +5,12 @@ set -e
 # Do whatever it takes to get up to date obs on the remote machines.
 # Note: developers use apt or brew packages on ubuntu and mac, but buildslaves just install obs from git.
 
-for slave in `./slaves.sh`
+for slave in `./slaves.sh | grep -v win | grep -v pi3`
 do
     echo ===== $slave ====
-    ssh -o StrictHostKeyChecking=no buildbot@${slave} "cd src/obs && git checkout master && git pull --ff-only; cd ~/.obs/obs && git pull --ff-only" || true
+    ssh -o StrictHostKeyChecking=no buildbot@${slave} "sh .obs/obs/buildbot/bootstrap-obs.sh" &
+    sleep 1
 done
+wait
 
 echo done
