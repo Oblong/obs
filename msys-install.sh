15bff6b7b0257f1ad653d5ad586a9eba68a0d4fb
Dan Kegel <dank@oblong.com>
2020-04-15 21:10:27 -0700

    Version 1.50

--- msys-install.sh
+++ msys-install.sh
@@ -12,7 +12,7 @@ fi
 
 # This is version major * 1000 + version minor
 # Must edit VERSIONOID in both Makefile and msys-install.sh
-VERSIONOID=1049
+VERSIONOID=1050
 
 # bau: bau.in
 echo VERSIONOID is ${VERSIONOID}
